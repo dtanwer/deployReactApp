da1e08cc3fba0da943cc5ccc71f3bd68f7e5e4c1
Deepak <tanwer644d@gmail.com>
2024-03-01 12:10:27 +0530

    Commented out console.log and res.sendFile in reactRouter.js

--- routes/reactRouter.js
+++ routes/reactRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 router.get("*", (req, res) => {
-    console.log("path",path.resolve(__dirname,"..", 'my-base-pathcheck', 'build', 'index.html'));
+    // console.log("path",path.resolve(__dirname,"..", 'my-base-pathcheck', 'build', 'index.html'));
     res.send("Api is running")
     // res.sendFile(path.resolve(__dirname,"..", 'my-base-pathcheck', 'build', 'index.html'));
 })
