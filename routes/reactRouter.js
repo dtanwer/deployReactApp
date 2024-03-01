014b8c827155f4edd0ccff2849ecc5542e932114
Deepak <tanwer644d@gmail.com>
2024-03-01 12:08:19 +0530

    Update reactRouter.js to send "Api is running" instead of serving index.html

--- routes/reactRouter.js
+++ routes/reactRouter.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const path = require('path');
 router.get("*", (req, res) => {
     console.log("path",path.resolve(__dirname,"..", 'my-base-pathcheck', 'build', 'index.html'));
-    res.sendFile(path.resolve(__dirname,"..", 'my-base-pathcheck', 'build', 'index.html'));
+    res.send("Api is running")
+    // res.sendFile(path.resolve(__dirname,"..", 'my-base-pathcheck', 'build', 'index.html'));
 })
 
 module.exports = router;
